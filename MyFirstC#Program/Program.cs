2c445f63a0cf295e05d02f2fc213f5cd8810ec64
ThanhTumg <thanhtumg.2510@gmail.com>
2025-10-23 15:17:04 +0700

    Add project files.

---
+++ MyFirstC#Program/Program.cs
@@ -0,0 +1,35 @@
+﻿// C# Tutorial
+/*
+Console.WriteLine("Enter side A: ");
+double sideA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter side B: ");
+double sideB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("The hypotenuse is: " + Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2)));
+*/
+
+/* String Manipulation in C#
+String fullName = "Thanh Tung";
+String phoneNumber = "0123-456-789";
+FullName = FullName.ToUpper();
+FullName = FullName.ToLower();
+Console.WriteLine(fullName);
+
+phoneNumber = phoneNumber.Replace("-", "/");
+Console.WriteLine(phoneNumber);
+
+String userName = fullName.Insert(0, "Mr. ");
+Console.WriteLine(userName);
+
+int len = fullName.Length;
+Console.WriteLine("Length of fullName: " + len);
+
+String firstName = fullName.Substring(0, 5);
+String lastName = fullName.Substring(6);
+Console.WriteLine("First Name: " + firstName);
+Console.WriteLine("Last Name: " + lastName);
+*/
+
+
+Console.ReadLine();
